711a4adceac554412eb492cfa7a5d7c4308944cb
jimmayhugh <jimmayhugh@gmail.com>
2014-02-21 23:34:59 -0800

    added find devices

--- index.php
+++ index.php
@@ -18,6 +18,142 @@
     <?php 
       include_once("accessDatabase.php");
       include_once("udpRequest.php");
+      
+      if( (isset($_POST["checknew"]) && $_POST["checknew"] === "checknew") )
+      {
+      //  echo "<h1> exec command </h1>";
+        $execCmd = "avahi-browse -t --resolve _discover._udp | egrep '(hostname|address)'";
+        $execArray = array();
+        exec($execCmd, $execArray);
+        $arrayCnt = count($execArray);
+      //          echo "<br />$arrayCnt elements in \$execArray<br />";
+
+        $execArrayCnt = 0;
+        $bonjourArray = array();
+        foreach($execArray as $key => $value)
+        {
+          if(($execArrayCnt % 2) === 0)
+          {
+            $bonjourName = current(explode(".", findText("[", "]", $value)));
+      //      echo "\$bonjourName = $bonjourName<br />";
+          }else{
+            $ipAddr = findText("[", "]", $value);
+      //      echo "\$ipAddr = $ipAddr<br />";
+            $bonjourArray[$bonjourName] = $ipAddr;
+          }
+          $execArrayCnt++;
+        }
+
+      /*  
+        foreach($bonjourArray as $key => $value)
+        {
+          echo $key."'s ip address is ".$value."<br />";
+        }
+      */  
+        $port_address = 2652;
+        $dvCnt = 0;
+        $pageArray = [
+                      ["Sensor Status", "SensorStatus.php"],
+                      ["Action Status", "ActionStatus.php"],
+                      ["PID Status" ,"PidStatus.php"],
+                      ["Check For New / Restore" , "CheckForNewRestore.php"],
+                      ["Update Names", "UpdateNames.php"]
+                     ];
+        $devStr = "<table width=\"100%\" border=\"1\" cellspacing=\"0\" cellpadding=\"2\"><tr>";
+        foreach($bonjourArray as $key => $value)
+        {
+          $dvNetworkDevicesStr = "SELECT * FROM `netDevices` WHERE service_port='$value'";
+          $dvResult = mysqli_query($link, $dvNetworkDevicesStr);
+          $dvNetworkCnt = mysqli_num_rows($dvResult);
+      //    echo "$dvNetworkCnt device(s) found at $value <br />";
+
+      /**** Programming Note ****
+
+        I realize that the following switch statement looks a little weird,
+        but it saves some programming and is perfectly legal.
+        If there's more than one entry for a given IP address in the MySQL table,
+        we're goining to delete all of the entries and start fresh.
+
+      ***** Programming Note ***/
+              
+          switch($dvNetworkCnt)
+          {
+            default: 
+            {
+              $dvDeleteStr = "DELETE FROM `netDevices` WHERE `service_port`='$value'";
+      //        echo "<br />\$dvDeleteStr = $dvDeleteStr<br />";
+              $dvDeleteResult = mysqli_query($link, $dvDeleteStr);
+              mysqli_free_result($dvDeleteResult);
+            } // fall through to case 0
+
+            case 0:
+            {
+              $dvInsertStr = "INSERT INTO `netDevices`(`netName`, `service_port`, `port_address`, `netActive`) VALUES ('$key','$value','2652','1')";
+      //        echo "<br />\$dvInsertStr = $dvInsertStr<br />";
+              $dvInsertResult = mysqli_query($link, $dvInsertStr);
+              mysqli_free_result($dvInsertResult);
+              mysqli_free_result($dvResult);
+              $dvNetworkDevicesStr = "SELECT * FROM `netDevices` WHERE service_port='$value'";
+              $dvResult = mysqli_query($link, $dvNetworkDevicesStr);
+            } //fall through to update screen
+            
+/*            case 1:
+            {
+              $dvCnt++;
+              $dvObject = mysqli_fetch_object($dvResult);
+              $dvIn = $getChipCount."\n";
+              $dvChipCount = udpRequest($value, $dvObject->port_address, $dvIn);
+              $dvIn = $versionID."\n";
+              $dvVersionID = udpRequest($value, $dvObject->port_address, $dvIn);
+              $devStr .= 
+                "<td align = \"center\"><h2><font color=\"blue\">".$key."</font><br /><h2>Chip Count: ".$dvChipCount."</h2>";
+              foreach($pageArray as $pageData)
+              {
+                $devStr .= 
+                "<form method=\"post\" action=\"".$pageData[1]."\">\n";
+                if($pageData[0] === "Update Names")
+                {
+                  $devStr .= 
+                  "<input type=\"hidden\" name=\"stopUpdate\" value=\"1\">\n";
+                }else{
+                  $devStr .= 
+                  "<input type=\"hidden\" name=\"stopUpdate\" value=\"0\">\n";
+                }
+                $devStr .= 
+                  "<input type=\"hidden\" name=\"netID\" value=\"".$dvObject->netID."\">
+                  <input type=\"hidden\" name=\"netName\" value=\"".$dvObject->netName."\">
+                  <input type=\"hidden\" name=\"service_port\" value=\"".$dvObject->service_port."\">
+                  <input type=\"hidden\" name=\"port_address\" value=\"".$dvObject->port_address."\">
+                  <input type=\"submit\" value=\"".$pageData[0]."\" style=\"height:35px; width:300px\">
+                </form><br />";
+              }
+              $devStr .= $dvVersionID."</center></td>";
+            }
+*/
+          }
+        }
+        mysqli_free_result($dvResult);
+        $devStr .= "</tr></table>";
+      }
+
+      function findText($start_limiter,$end_limiter,$haystack)
+      {
+         $start_pos = strpos($haystack,$start_limiter);
+         if ($start_pos === FALSE)
+         {
+             return FALSE;
+         }
+
+         $end_pos = strpos($haystack,$end_limiter,$start_pos);
+
+         if ($end_pos === FALSE)
+         {
+            return FALSE;
+         }
+
+         return substr($haystack, $start_pos+1, ($end_pos-1)-$start_pos);
+      }
+
     ?>
     </td>
   </tr>
@@ -69,6 +205,11 @@
       {
         $dvIn = $getChipCount."\n";
         $dvChipCount = udpRequest($dvRow[$service_port], $dvRow[$port_address], $dvIn);
+//        echo "\$dvChipCount = $dvChipCount<br />";
+        if($dvChipCount === "")
+        {
+          $dvChipCount = "<font color=\"red\"><strong>OFFLINE</strong></font>";
+        }
         $dvIn = $versionID."\n";
         $dvVersionID = udpRequest($dvRow[$service_port], $dvRow[$port_address], $dvIn);
         $formStr .= 
