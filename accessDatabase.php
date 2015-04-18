9253b6f2495b4dd8e5ebb1d6aa2ea666ac9a8e3f
Jim Mayhugh <jimmayhugh@gmail.com>
2015-04-18 09:02:42 -0700

    cleaning up

--- accessDatabase.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username="teensynet";
-$password="teensynet";
-$database="teensynet";
-
-$link = mysqli_connect("localhost",$username,$password);
-@mysqli_select_db($link,$database) or die( "Unable to select database");
-
-?>
